42fe8ad698edb943de1cd6cdf877689c2fd050d7
Marcelo Inácio <103380466+MarceloInacioJr@users.noreply.github.com>
2024-04-07 21:56:57 -0300

    Update configFirebase.js

--- src/db/configFirebase.js
+++ src/db/configFirebase.js
@@ -5,9 +5,5 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import { getDatabase } from "firebase/database";
 
-
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app)
 export const auth = getAuth() // !importante 
 
