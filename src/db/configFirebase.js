3aedb2457c5b23c96ceff94f36aca566a42fc9c6
Marcelo Inácio <103380466+MarceloInacioJr@users.noreply.github.com>
2024-04-07 21:23:11 -0300

    Update configFirebase.js
    Ajust  credentiais

--- src/db/configFirebase.js
+++ src/db/configFirebase.js
@@ -8,13 +8,13 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0CbLF6P2vNk7L-BSY5L-nZ7LvwX9SkR4",
+  apiKey: "4",
   authDomain: "todolist-1a4d1.firebaseapp.com",
-  projectId: "todolist-1a4d1",
-  storageBucket: "todolist-1a4d1.appspot.com",
-  messagingSenderId: "418275952972",
-  appId: "1:418275952972:web:9dffca7e422c812ea267e9",
-  measurementId: "G-ZX4X8D10BS"
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
